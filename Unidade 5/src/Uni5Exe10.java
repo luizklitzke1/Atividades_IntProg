397ae5c3052f262867713212fc7351c5a58eacb7
Luiz Gustavo Klitzke <49824305+luizklitzke1@users.noreply.github.com>
2021-05-16 14:53:09 -0300

    uni510 com real qualquer

--- Unidade 5/src/Uni5Exe10.java
+++ Unidade 5/src/Uni5Exe10.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Uni5Exe10 {
 
     public static void main(String[] args) throws Exception {
@@ -9,6 +8,7 @@ public class Uni5Exe10 {
         int quantidadeEncontrada = 0;
         int potencia = 0;
 
+        System.out.println("[Assumindo que sejam apeans com 4 digitos]");
         for (int numeroTeste = 1000; numeroTeste < 9999; numeroTeste++)
         {
             potencia = (numeroTeste / 100) + (numeroTeste % 100);
@@ -21,6 +21,27 @@ public class Uni5Exe10 {
 
                 if (quantidadeEncontrada == 10)
                     break;
+            }
+        }
+
+
+        quantidadeEncontrada = 0;
+        potencia = 0;
+        int metade = 0;
+
+        //Nao necessariamente seguindo o apresentando em aula, porem resolvendo o proposto
+        System.out.println("\n[Assumindo que seja a potencia de (metade + metade) de um real qualquer]");
+        for (int numeroTeste = 1000; quantidadeEncontrada < 10; numeroTeste++)
+        {
+            metade = Integer.toString(numeroTeste).length() / 2;
+
+            potencia = (numeroTeste / (int)Math.pow(10, metade)) + (numeroTeste % (int)Math.pow(10, metade));
+
+            if (potencia * potencia == numeroTeste)
+            {
+                quantidadeEncontrada++;
+
+                System.out.println("[" + quantidadeEncontrada + "] " + numeroTeste);
             }
         }
        
