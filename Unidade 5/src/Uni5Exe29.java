565e1541089e84e7f32b0f4af5fdc565c604c65f
Luiz Gustavo Klitzke <49824305+luizklitzke1@users.noreply.github.com>
2021-05-29 21:45:41 -0300

    Mudada au5 29 pra usar while...  why?

--- Unidade 5/src/Uni5Exe29.java
+++ Unidade 5/src/Uni5Exe29.java
@@ -9,22 +9,41 @@ public class Uni5Exe29 {
         System.out.print("Informe o valor R$ " );
 
         int val = tec.nextInt();
-        int temp = val;
 
-        int notas20 = temp / 20;
-        temp -= notas20 * 20;
+        int notas20 = 0;
+        int notas10 = 0;
+        int notas5 = 0;
+        int notas2 = 0;
+        int notas1 = 0;
 
-        int notas10 = temp / 10;
-        temp -= notas10 * 10;
-
-        int notas5 = temp / 5;
-        temp -= notas5 * 5;
-
-        int notas2 = temp / 2;
-        temp -= notas2 * 2;
-
-        int notas1 = temp / 1;
-        temp -= notas1 * 1;
+        while (val > 0)
+        {
+            if (val >= 20)
+            {
+                notas20++;
+                val -= 20;
+            }
+            else if (val >= 10)
+            {
+                notas10++;
+                val -= 10;
+            }
+            else if (val >= 5)
+            {
+                notas5++;
+                val -= 5;
+            }
+            else if (val >= 2)
+            {
+                notas2++;
+                val -= 2;
+            }
+            else
+            {
+                notas1++;
+                val--;
+            }
+        }
 
         System.out.println( notas20 + " nota(s) de R$ 20,00" );
         System.out.println( notas10 + " nota(s) de R$ 10,00" );
